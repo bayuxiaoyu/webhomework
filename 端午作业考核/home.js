1aecd5299718cb0e509ec61a089200f54c3459d1
bayuxiaoyu <2782303429@qq.com>
2022-06-04 21:01:25 +0800

    3

--- 端午作业考核/home.js
+++ 端午作业考核/home.js
@@ -14,11 +14,24 @@ window.onload = function(){
     rankbtn.onclick = function(){
         recommend.style.display = 'none';
         rank.style.display = 'flex';
+        document.getElementById("recommendbtn").disabled = false;
     }
 
     searchbtn.onclick = function(){
         recommend.style.display = 'none';
         hotsearch.style.display = 'block';
+        hotbox.style.display = 'block';
+
+        for(let i=0;i<10;i++){
+            outli[i].innerHTML = `
+            <div class="outname"></div>
+            <div class="outartist"></div>
+            `
+        }
+
+        outbox.style.display = 'none';
+        search.value = '';
+        document.getElementById("recommendbtn").disabled = true;
     }
 
 
@@ -89,14 +102,116 @@ window.onload = function(){
 
 //搜索
 
+    const cancel = document.getElementById("cancel");
+    const hotli = document.getElementsByClassName("hotli");
+
+    cancel.onclick = function(){
+        hotsearch.style.display = 'none';
+        recommend.style.display = 'block';
+        document.getElementById("recommendbtn").disabled = false;
+    }
+
+    async function gethot(){
+        let res = await fetch("http://124.221.249.219:8000/api/hot",{
+        method:"GET",
+        })
+        let asd = res.json();
+        //console.log(asd);
+
+        asd.then(data =>{
+           //console.log("数据是",data);
+            for(let i=0;i<10;i++){
+                hotli[i].innerHTML = data[i];
+            }
+
+
+        })
+
+    }
+
+    gethot();
+
+
+//搜索出来
+
+    const search  = document.getElementById("search");
+    const outli = document.getElementsByClassName("outli");
+    const hotbox = document.getElementById("hotbox");
+    const outbox = document.getElementById("outbox");
+    
+    search.onchange = function(){
+        document.onkeyup = (e) => {
+            const event = e || window.event
+            const key = event.which || event.keyCode || event.charCode
+            if (key === 13) {
+                hotbox.style.display = 'none';
+                outbox.style.display = 'block';
+                async function getout(){
+                    let res = await fetch("http://124.221.249.219:8000/api/search?keyword="+search.value+"",{
+                    method:"GET",
+                    })
+                    let asd = res.json();
+                    //console.log(asd);
+            
+                    asd.then(data =>{
+                       console.log("数据是",data);
+                        
+                       for(let i=0;i<10;i++){
+                           outli[i].innerHTML = `
+                           <div class="outname">`+data[i].title +`</div>
+                           <div class="outartist">`+data[i].artist.toString() +`</div>
+
+
+                           `
+                       }
+
+
+
+
+
+
+
+
+
+                    })
+            
+                }
+            
+                getout();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+            document.onkeyup = null;
+    }
+    }
+
+
+
+
+
 
 
 //rank
 
     const rankli = document.getElementsByClassName("rankli");
-
-
-
+    
     async function getrank(){
         let res = await fetch("http://124.221.249.219:8000/api/ranking",{
         method:"GET",
@@ -105,7 +220,7 @@ window.onload = function(){
         //console.log(asd);
 
         asd.then(data =>{
-        console.log("数据是",data);
+        //console.log("数据是",data);
             for(let i=0;i<10;i++){
 
                 rankli[i].innerHTML = `
