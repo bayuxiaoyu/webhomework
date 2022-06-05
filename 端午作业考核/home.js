f6ca47c30abec96b969dd16c5d1fa422a5d21484
bayuxiaoyu <2782303429@qq.com>
2022-06-05 11:47:29 +0800

    4

--- 端午作业考核/home.js
+++ 端午作业考核/home.js
@@ -9,18 +9,29 @@ window.onload = function(){
     recommendbtn.onclick = function(){
         rank.style.display = 'none';
         recommend.style.display = 'block';
+        deletebtn.style.visibility = 'hidden';
+        hotsearch.style.display = 'none';
+        recommendbtn.style.color = 'rgb(99, 204, 169)';
+        rankbtn.style.color = 'rgb(189, 189, 189)';
     }
 
     rankbtn.onclick = function(){
         recommend.style.display = 'none';
         rank.style.display = 'flex';
+        deletebtn.style.visibility = 'hidden';
+        hotsearch.style.display = 'none';
         document.getElementById("recommendbtn").disabled = false;
+        rankbtn.style.color = 'rgb(99, 204, 169)';
+        recommendbtn.style.color = 'rgb(189, 189, 189)';
     }
 
     searchbtn.onclick = function(){
         recommend.style.display = 'none';
         hotsearch.style.display = 'block';
         hotbox.style.display = 'block';
+        historybox.style.display  = "block";
+        historybox.style.display  = "block";
+        showhistory();
 
         for(let i=0;i<10;i++){
             outli[i].innerHTML = `
@@ -54,7 +65,7 @@ window.onload = function(){
                 guanfangli[i].innerHTML = `
             <div class="guanfangpic">
                 <img src="`+data.offical[i].cover +`" width="100px" height="100px" class="guanfangcover">
-                <div class="guanfangnum">▶`+data.offical[i].views +`</div>
+                <div class="guanfangnum">&nbsp;&nbsp;▶`+data.offical[i].views +`&nbsp;&nbsp;  </div>
             </div>
             <div class="guanfangdes">
                `+data.offical[i].title +`
@@ -65,7 +76,7 @@ window.onload = function(){
                 darenli[i].innerHTML = `
             <div class="darenpic">
                 <img src="`+data.tatsujin[i].cover +`" width="100px" height="100px" class="darencover">
-                <div class="guanfangnum">▶`+data.tatsujin[i].views +`</div>
+                <div class="guanfangnum">&nbsp;&nbsp;▶`+data.tatsujin[i].views +`&nbsp;&nbsp;</div>
             </div>
             <div class="darendes">
             `+data.tatsujin[i].title +`
@@ -104,10 +115,20 @@ window.onload = function(){
 
     const cancel = document.getElementById("cancel");
     const hotli = document.getElementsByClassName("hotli");
+    let history = [];
+    const historytags = document.getElementById("historytags");
+
+    function showhistory(){
+        historytags.innerHTML = '';
+        for(let i=0;i<history.length;i++){
+            historytags.innerHTML += `<span class="historyli">`+history[i] +`</span>`;
+        }
+    }
 
     cancel.onclick = function(){
         hotsearch.style.display = 'none';
         recommend.style.display = 'block';
+        deletebtn.style.visibility = 'hidden';
         document.getElementById("recommendbtn").disabled = false;
     }
 
@@ -135,9 +156,11 @@ window.onload = function(){
 //搜索出来
 
     const search  = document.getElementById("search");
+    const deletebtn = document.getElementById("deletebtn");
     const outli = document.getElementsByClassName("outli");
     const hotbox = document.getElementById("hotbox");
     const outbox = document.getElementById("outbox");
+    const historybox = document.getElementById("historybox");
     
     search.onchange = function(){
         document.onkeyup = (e) => {
@@ -145,7 +168,10 @@ window.onload = function(){
             const key = event.which || event.keyCode || event.charCode
             if (key === 13) {
                 hotbox.style.display = 'none';
+                historybox.style.display = 'none';
                 outbox.style.display = 'block';
+                history.push(search.value);
+                
                 async function getout(){
                     let res = await fetch("http://124.221.249.219:8000/api/search?keyword="+search.value+"",{
                     method:"GET",
@@ -154,7 +180,7 @@ window.onload = function(){
                     //console.log(asd);
             
                     asd.then(data =>{
-                       console.log("数据是",data);
+                       //console.log("数据是",data);
                         
                        for(let i=0;i<10;i++){
                            outli[i].innerHTML = `
@@ -167,41 +193,32 @@ window.onload = function(){
 
 
 
-
-
-
-
-
-
                     })
             
                 }
             
                 getout();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             }
 
             document.onkeyup = null;
     }
+
+        if(search.value!=''){
+            deletebtn.style.visibility = 'visible';
+        }else{
+            deletebtn.style.visibility = 'hidden';
+        }
+
     }
 
+    deletebtn.onclick = function(){
+        search.value = '';
+        historybox.style.display  = "block";
+        outbox.style.display = 'none';
+        hotbox.style.display = 'block';
+        showhistory();
+    }
 
 
 
@@ -226,15 +243,15 @@ window.onload = function(){
                 rankli[i].innerHTML = `
             <div class="left">
                 <div class="rankname">`+data[i].title +`</div>
-                    <div><span class="songname">1.`+data[i].top3[0].title +`</span><span class="artist">-`+data[i].top3[0].artist.toString() +`</span></div> 
-                    <div><span class="songname">2.`+data[i].top3[1].title +`</span><span class="artist">-`+data[i].top3[1].artist.toString() +`</span></div> 
-                    <div><span class="songname">3.`+data[i].top3[2].title +`</span><span class="artist">-`+data[i].top3[2].artist.toString()  +`</span></div>
+                    <div class="one"><span class="songname">1.`+data[i].top3[0].title +`</span><span class="artist">-`+data[i].top3[0].artist.toString() +`</span></div> 
+                    <div class="one"><span class="songname">2.`+data[i].top3[1].title +`</span><span class="artist">-`+data[i].top3[1].artist.toString() +`</span></div> 
+                    <div class="one"><span class="songname">3.`+data[i].top3[2].title +`</span><span class="artist">-`+data[i].top3[2].artist.toString()  +`</span></div>
             </div>
 
             <div class="right">
                 <img src="`+data[i].cover +`" width="110px" height="110px">
-                <span class="update">每`+data[i].update_frequence +`更新</span>
-                <span class="listennum">▶`+data[i].views +`</span>
+                <span class="update">&nbsp;&nbsp;每`+data[i].update_frequence +`更新&nbsp;&nbsp;</span>
+                <span class="listennum">&nbsp;&nbsp;▶`+data[i].views +`&nbsp;&nbsp;</span>
             </div>
                 
                 
